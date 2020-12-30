fe2d99bbdfb711c25139ff6f904aa1a8daf62008
darkdog1974 <darkdog74@hotmail.com>
2020-12-30 19:20:56 +0100

    4 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component, VERSION } from '@angular/core';
+import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'my-app',
@@ -7,4 +9,12 @@ import { Component, VERSION } from '@angular/core';
 })
 export class AppComponent  {
   name = 'Angular ' + VERSION.major;
+  utentiCollection: AngularFirestoreCollection<any>;
+  utenti: Observable<any[]>;
+ 
+
+  constructor(public afs: AngularFirestore) {
+    this.utentiCollection = this.afs.collection("UTENTI");
+    this.utenti = afs.collection("UTENTI").valueChanges();
+  }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,22 +1,12 @@
 export const environment = {
   production: false,
   firebase: {
-    /*
-    apiKey: "AIzaSyAR9XS_X8puHt60rJGNR0DXmOrWFiEjwoU",
-    authDomain: "mike-goodstadt.firebaseapp.com",
-    databaseURL: "https://mike-goodstadt.firebaseio.com",
-    projectId: "mike-goodstadt",
-    storageBucket: "mike-goodstadt.appspot.com",
-    messagingSenderId: "610369153792",
-    appId: "1:610369153792:web:48900a10f5399cbf"*/
-
     apiKey: "AIzaSyDsDcX3Nol9y8he1f2KPNhxoQFcOpE5ew8",
     authDomain: "angularfirebaseexample-f3651.firebaseapp.com",
     projectId: "angularfirebaseexample-f3651",
     storageBucket: "angularfirebaseexample-f3651.appspot.com",
     messagingSenderId: "299480724840",
     appId: "1:299480724840:web:09fbf1b709eeb4efcad2dd",
-    databaseURL:
-      "https://angularfirebaseexample-f3651-default-rtdb.europe-west1.firebasedatabase.app/"
+    databaseURL: "https://angularfirebaseexample-f3651.firebaseio.com"
   }
 };
